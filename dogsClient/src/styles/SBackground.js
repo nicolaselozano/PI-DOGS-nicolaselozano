c5c214c3282f787db7d4ddb497dffb2f506f4dd8
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2023-12-08 18:24:31 -0300

    cambios cliente para deploy default port 6

--- dogsClient/src/styles/SBackground.js
+++ dogsClient/src/styles/SBackground.js
@@ -12,7 +12,8 @@ export const SBackground = styled.div.attrs((props)=> ({
   justify-content: center;
   text-align: center;
   background-image: url(${backgroundURLimg});
-  min-height: auto;
+  height: 100vh;
+  min-height: 100vh; 
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;

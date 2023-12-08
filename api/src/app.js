155cbb6870a90d5357f4a53393b58cba0a6f2d17
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2023-12-08 17:24:28 -0300

    cambios api deploy

--- api/src/app.js
+++ api/src/app.js
@@ -16,7 +16,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://dog-santuary-client.onrender.com'); // REEMPLAZAR POR  http://localhost:5173 SI ESTAS DE FORMA LOCAL update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
